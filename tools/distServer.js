dc97cb73a13758f7aa08aa5b66c9c808ab9e04a4
Richard Mands <richard@eduku.org>
2017-05-11 16:03:56 +0800

    update

--- tools/distServer.js
+++ tools/distServer.js
@@ -11,15 +11,16 @@ console.log(chalkProcessing('Opening production build...'));
 
 // Run Browsersync
 browserSync({
-  port: 4000,
+  port: (process.env.PORT || 4000),
   ui: {
-    port: 4001
+    port: (process.env.PORT2 || 4001)
   },
   server: {
     baseDir: 'dist'
   },
 
   files: [
+    // 'dist/*.html'
     'src/*.html'
   ],
 
